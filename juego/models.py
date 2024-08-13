d415a4a478c48b11973bae890381679d104667d8
Cristian <cristian3087@hotmail.com>
2024-08-13 16:59:16 -0500

    [FEAT] Temporizador fetch

--- juego/models.py
+++ juego/models.py
@@ -7,13 +7,16 @@ from usuarios.models import BaseModel, Jugador
 #     aleas = models.IntegerField(default=1)
 
 NIVEL = (
-    ('1', 'BASICO'),
-    ('2', 'MEDIO'),
-    ('3', 'AVANZADO'),
-    ('4', 'EXPERTO'),
+    (1, 'BASICO'),
+    (2, 'MEDIO'),
+    (3, 'AVANZADO'),
+    (4, 'EXPERTO'),
 )
 class Categoria(BaseModel):
     nombre = models.CharField(max_length=60)
+    
+    def __str__(self) -> str:
+        return f"{self.id}.{self.nombre}"
   
 
 
@@ -21,11 +24,15 @@ class Pregunta(BaseModel):
     enunciado= models.TextField()
     nivel = models.IntegerField(choices=NIVEL)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return f"{self.enunciado}, {self.nivel}, {self.categoria.nombre}"
 
 class Respuesta(BaseModel):
     pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
     texto = models.TextField()
     es_correcta = models.BooleanField(default=False)
+    def __str__(self):
+        return f"{self.pregunta.id}, {self.texto}, {self.es_correcta}"     
 
 class ConfiguracionPartida(BaseModel):
     num_preguntas = models.IntegerField(default=1)
--- juego/views.py
+++ juego/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def juego_trivia(request):
+    print(request.method)
+    print(request.POST)
     return render(request, 'juego/trivia.html', {})
